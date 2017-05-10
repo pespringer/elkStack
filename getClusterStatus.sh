e7fb188c5aa2c5ae6239b40bcf457b2972eb3f78
Patrick Springer <pspringer@localhost.localdomain>
2017-05-09 22:49:20 -0400

    Added retry loop logic for Elk stack status and verification

--- getClusterStatus.sh
+++ getClusterStatus.sh
@@ -3,24 +3,33 @@
 STATUS=`curl --silent -u elastic:changeme http://127.0.0.1:9200/_cat/health`
 
 restartElkStack () {
-  sudo /usr/local/bin/docker-compose down
-  sudo /usr/local/bin/docker-compose up -d
-  sleep 15
-  if [ $? -ne 0 ]
-    then
-      echo "ELK restart failed..."
- fi
+curl --silent -u elastic:changeme http://127.0.0.1:9200/_cat/health
+  /usr/local/bin/docker-compose down
+  /usr/local/bin/docker-compose up -d
+  counter=0
+  retry=30
+  while [ ! "$(curl -u elastic:changeme http://127.0.0.1:9200 2> /dev/null)" -a $counter -lt $retry  ]; do
+    sleep 1
+    ((counter++))
+    echo "Waiting for Elk cluster to start. ($counter/$retry)"
+  done
+  if [ ! "$(curl -u elastic:changeme http://127.0.0.1:9200 2> /dev/null)" ]; then
+    echo "Couln't start Elk cluster. Exiting."
+    exit 1
+  else
+    echo "Cluster status: `curl --silent -u elastic:changeme http://127.0.0.1:9200/_cat/health`"
+  fi
 }
 
 checkClusterStatus() {
-if [ `echo ${STATUS} | grep docker-cluster | wc -l` -ne 1 ]
-  then
-    echo "ELK Cluster is not running..."
-    echo "Attempting to restart ELK stack..."
-    restartElkStack
-  else
-    echo "Cluster ${STATUS}"
-fi
+  if [ `echo ${STATUS} | grep docker-cluster | wc -l` -ne 1 ]
+    then
+      echo "ELK Cluster is not running..."
+      echo "Attempting to restart ELK stack..."
+      restartElkStack
+    else
+      echo "Cluster ${STATUS}"
+  fi
 }
 
 checkClusterStatus
